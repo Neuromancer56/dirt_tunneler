d7c3ab82cbb7eea49464150c613a0e3519b3901f
Neuromancer56 <neuromancer56@gmail.com>
2024-04-06 18:07:49 -0500

    adds ability to go tunnel up with auto-ladders

--- nodes.lua
+++ nodes.lua
@@ -20,6 +20,7 @@ local function dirtTouchAction(player)
 	--minetest.log("x","x_start:"..x_start..", x_end:"..x_end)	
 	--minetest.log("x","z_start:"..z_start..", z_end:"..z_end)	
 	local dirt_dug = false
+	local dirt_dug_above = false
 
 	for dx = x_offset, x_offset do
 		--minetest.log("x","dx:"..dx)	
@@ -31,12 +32,25 @@ local function dirtTouchAction(player)
 				
 				if (node.name == "default:dirt" or node.name == "default:dry_dirt" )then
 					minetest.set_node(neighbor_pos, {name = "air", param2 = node.param2})
-					dirt_dug = true
-							
+					dirt_dug = true	
+				end
+				local above_pos = {x = pos.x, y = pos.y + 1.8, z = pos.z}  --1.75-2.0 sound but no dig 1.7 no sound & no dig 1.72 is sporadic.
+				local above_node = minetest.get_node(above_pos)
+				if (above_node.name == "default:dirt" or above_node.name == "default:dry_dirt" )then
+					minetest.set_node(above_pos, {name = "default:ladder", param2 = 4})
+					minetest.set_node(pos, {name = "default:ladder", param2 = 4})
+					--minetest.set_node(above_pos, {name = "default:ladder_steel", param2 = 4})
+					dirt_dug_above = true	
+					--minetest.log("x","dirt_dug_above:"..tostring(dirt_dug_above))
 				end
 			end
 		end
 	end
+	if dirt_dug_above then
+		minetest.sound_play("default_dig_crumbly", {pos = pos, gain = 0.5, max_hear_distance = 10}) 
+
+	end
+	
     if dirt_dug then 
 		minetest.sound_play("default_dig_crumbly", {pos = pos, gain = 0.5, max_hear_distance = 10}) 
 		
