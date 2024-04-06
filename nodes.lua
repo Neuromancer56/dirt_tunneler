7eeff05ec8b9e70f9849b7f93f1e8f8fab0cc57b
Neuromancer56 <neuromancer56@gmail.com>
2024-04-06 13:06:22 -0500

    Consolidated duplicate code

--- nodes.lua
+++ nodes.lua
@@ -15,42 +15,16 @@ end
 
 local function dirtTouchAction(player)
 	local pos = player:get_pos()
-	--minetest.swap_node(pos, {name = "hero_mines:broken_mese_post_light", param2 = node.param2})
 
-	local yaw = player:get_look_horizontal()
-	local yaw_degrees = (yaw + 2 * math.pi) % (2 * math.pi) * 180 / math.pi
-	--minetest.log("x","yaw_degrees:"..yaw_degrees)	
-
-	local x_start = 0
-	local x_end = 0
-
-	local z_start = 0
-	local z_end = 0
-
-	if yaw_degrees >= 225 and yaw_degrees < 315 then
-		x_end = 1
-		x_start = 1
-	elseif yaw_degrees >= 45 and yaw_degrees < 135 then
-		x_start = -1
-		x_end = -1
-	end
-
-	if yaw_degrees >= 315 or yaw_degrees < 45 then
-		   z_end = 1
-		z_start = 1
-	elseif yaw_degrees >= 135 and yaw_degrees < 225 then
-		   z_end = -1
-		z_start = -1
-	end
-
+	local x_offset, z_offset= get_direction_offsets(player)
 	--minetest.log("x","x_start:"..x_start..", x_end:"..x_end)	
 	--minetest.log("x","z_start:"..z_start..", z_end:"..z_end)	
 	local dirt_dug = false
 
-	for dx = x_start, x_end do
+	for dx = x_offset, x_offset do
 		--minetest.log("x","dx:"..dx)	
 		for dy = 0, 1 do
-			for dz = z_start, z_end do
+			for dz = z_offset, z_offset do
 				--minetest.log("x","dz:"..dz)	
 				local neighbor_pos = {x = pos.x + dx, y = pos.y + dy, z = pos.z + dz}
 				local node = minetest.get_node(neighbor_pos)
@@ -66,10 +40,10 @@ local function dirtTouchAction(player)
     if dirt_dug then 
 		minetest.sound_play("default_dig_crumbly", {pos = pos, gain = 0.5, max_hear_distance = 10}) 
 		
-		for dx = x_start, x_end do
+		for dx = x_offset, x_offset do
 			--minetest.log("x","dx:"..dx)	
 			for dy = 1, 2 do
-				for dz = z_start, z_end do
+				for dz = z_offset, z_offset do
 					--minetest.log("x","dz:"..dz)	
 					local neighbor_pos = {x = pos.x + dx, y = pos.y + dy, z = pos.z + dz}
 					local node = minetest.get_node(neighbor_pos)
@@ -77,11 +51,6 @@ local function dirtTouchAction(player)
 				end
 			end
 		end
-		--[[if minetest.get_modpath("boulders") then
-		   minetest.after(.8, function()
-			check_for_tumbling(pos)
-			end)
-		end]]
 	end
 		--minetest.log("x","x:"..neighbor_pos.x ..",z:"..neighbor_pos.z)		
 	
